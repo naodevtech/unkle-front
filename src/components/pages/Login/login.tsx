21d58630a23670473e8363e858d99bb8fd694b6f
naodevtech <naimtahouri@gmail.com>
2022-04-19 17:19:40 +0200

    :pushpin: (features): tooltip added with input dynamic :pushpin:

--- src/components/pages/Login/login.tsx
+++ src/components/pages/Login/login.tsx
@@ -1,30 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../../../assets/logos/logo_unkle_version.svg";
+
+import Tooltip from "../../atoms/Tooltip/tooltip";
 import "./_login.scss";
 
 function Login() {
+  const [fields, setFields] = useState([
+    {
+      id: 0,
+      type: "email",
+      name: "email",
+      label: "Votre adresse e-mail",
+      placeholder: "Tapez votre adresse e-mail",
+      regexp: /^[a-zA-Z0-9-.]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/,
+      errorDisplayed: "Veuillez entrer une adresse e-mail correcte !",
+      error: false,
+    },
+    {
+      id: 1,
+      type: "password",
+      name: "password",
+      label: "Votre mot de passe",
+      placeholder: "Tapez votre mot de passe",
+      regexp: `^.{1,}$`,
+      errorDisplayed: "Veuillez entrer votre mot de passe !",
+      error: false,
+    },
+  ]);
+
+  const checkFieldValidity = (currentField: any, value: string) => {
+    const regexp = new RegExp(currentField.regexp);
+    if (!regexp.test(value)) {
+      setFields(
+        fields.map((field) =>
+          field.id === currentField.id ? { ...field, error: true } : field
+        )
+      );
+    } else {
+      setFields(
+        fields.map((field) =>
+          field.id === currentField.id ? { ...field, error: false } : field
+        )
+      );
+    }
+  };
+
+  const handleChange = (currentField: any, value: string) => {
+    checkFieldValidity(currentField, value);
+  };
+
   return (
     <div className="container_responsive">
       <div className="container_login">
         <img src={logo} alt="logo_unkle" />
         <form>
-          <div className="box_input">
-            <label className="body-s--light">Votre adresse e-mail</label>
-            <input
-              className="input"
-              type="email"
-              placeholder="Tapez votre adresse e-mail"
-            />
-          </div>
-          <div className="box_input">
-            <label className="body-s--light">Mot de passe</label>
-            <input
-              className="input"
-              type="password"
-              placeholder="Tapez votre mot de passe"
-            />
-          </div>
+          {fields
+            ? fields.map((field) => {
+                return (
+                  <div className="box_input" key={field.id}>
+                    <label className="body-s--light">{field.label}</label>
+                    <input
+                      className={`input ${
+                        field.error
+                          ? "input--icon-right--error error-border"
+                          : "null"
+                      }`}
+                      type={field.type}
+                      placeholder={field.placeholder}
+                      onChange={(e) => {
+                        handleChange(field, e.target.value);
+                      }}
+                    />
+                    {field.error ? (
+                      <Tooltip type={"error"} message={field.errorDisplayed} />
+                    ) : null}
+                  </div>
+                );
+              })
+            : null}
           <button className="btn btn--primary">Connexion</button>
         </form>
       </div>
